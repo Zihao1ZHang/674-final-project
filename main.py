b2183a1ddb87a6c0426631060865ff2cec9dc653
Zihao1ZHang <zihao.zhang1999@gmail.com>
2022-04-13 17:09:38 -0400

    create main utils and model file

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from utils import *
+import open3d as o3d
+import h5py
+import numpy as np
+import torch
+
+Config = {
+    "filedir": "./data/Completion3D/raw/train/partial/02691156/",
+    "train": "./tensor/train/02691156/",
+    "val": "./tensor/train/02691156/",
+    "test": "./tensor/train/02691156/",
+
+}
 
 if __name__ == '__main__':
-    filename = ""
-    data = load_data(filename)
-    
+    filename = "./data/Completion3D/raw/train/partial/02691156/1a04e3eab45ca15dd86060f189eb133.h5"
+    filename = "./data/Completion3D/raw/train/partial/02691156/1a9b552befd6306cc8f2d5fe7449af61.h5"
+
+    example_dt = h5py.File(filename, 'r')
+    key = list(example_dt.keys())[0]
+    data = torch.tensor(example_dt[key], dtype=torch.float32)
+    data_np = np.array(example_dt[key])
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(data)
+
+    o3d.visualization.draw_geometries([pcd])
+    print("sada")
+
--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 import os
 import numpy as np
+from skimage import measure
+import open3d as o3d
+import h5py
+
+
+def loop_dir(config):
+
+    return
+
 
 def load_data(filename):
     data = np.loadtxt(filename)
+    cloud = o3d.io.read_point_cloud(filename)  # Read the point cloud
+    o3d.visualization.draw_geometries([cloud])  # Visualize the point cloud
     return data
+
+
+def visualize_result(points):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    o3d.visualization.draw_geometries([pcd])
+    return
