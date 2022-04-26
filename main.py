8bdf06a36d28cd61cee4dde2f586326e6bb9163e
Zihao1ZHang <zihao.zhang1999@gmail.com>
2022-04-26 14:26:57 -0400

    load data

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 from utils import *
-import open3d as o3d
-import h5py
-import numpy as np
-import torch
+# from scipy.stats import wasserstein_distance
+# from chamfer_distance import ChamferDistance
 
-Config = {
-    "filedir": "./data/Completion3D/raw/train/partial/02691156/",
-    "train": "./tensor/train/02691156/",
-    "val": "./tensor/train/02691156/",
-    "test": "./tensor/train/02691156/",
 
-}
+
+def train(model, optimizer):
+    model.train()
+
+    return
+
 
 if __name__ == '__main__':
-    filename = "./data/Completion3D/raw/train/partial/02691156/1a04e3eab45ca15dd86060f189eb133.h5"
-    filename = "./data/Completion3D/raw/train/partial/02691156/1a9b552befd6306cc8f2d5fe7449af61.h5"
+    train_dataset = load_data("./data/Completion3D/raw/", "train", "Airplane")
+    val_dataset = load_data("./data/Completion3D/raw/", "val", "Airplane")
 
-    example_dt = h5py.File(filename, 'r')
-    key = list(example_dt.keys())[0]
-    data = torch.tensor(example_dt[key], dtype=torch.float32)
-    data_np = np.array(example_dt[key])
-
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(data)
-
-    o3d.visualization.draw_geometries([pcd])
     print("sada")
 
--- utils.py
+++ utils.py
@@ -3,14 +3,73 @@ import numpy as np
 from skimage import measure
 import open3d as o3d
 import h5py
+import torch
+from torch_geometric.data import Data
+from torch.utils.data import DataLoader
+import multiprocessing
+
+category_ids = {
+    'Airplane': '02691156',
+    'Bag': '02773838',
+    'Cap': '02954340',
+    'Car': '02958343',
+    'Chair': '03001627',
+    'Earphone': '03261776',
+    'Guitar': '03467517',
+    'Knife': '03624134',
+    'Lamp': '03636649',
+    'Laptop': '03642806',
+    'Motorbike': '03790512',
+    'Mug': '03797390',
+    'Pistol': '03948459',
+    'Rocket': '04099429',
+    'Skateboard': '04225987',
+    'Table': '04379243',
+}
 
 
-def loop_dir(config):
+def read_h5py(file):
+    data_list = []
 
-    return
+    with h5py.File(file, 'r') as f:
+        data_key = list(f.keys())[0]
+        data_np = np.array(f[data_key])
+        data = torch.tensor(data_np, dtype=torch.float32)
+        data_list.append(data)
+    return data_list
 
 
-def load_data(filename):
+def loop_dir(dir):
+    data_list = []
+    files = os.listdir(dir)
+    filenames = [dir + "/" + x for x in files]
+    core_num = 6
+    pool = multiprocessing.Pool(processes=core_num)
+    #result = pool.apply_async(read_h5py, filenames)
+    for data in enumerate(pool.imap(read_h5py, filenames)):
+        data_list.append(data)
+    pool.close()
+    pool.join()
+    return data_list
+
+
+def load_data(dir, phase, category):
+    assert(category in category_ids)
+    category = category_ids[category]
+    partial_dir = dir + phase + "/partial/" + category + "/"
+    gt_dir = dir + phase + "/gt/" + category + "/"
+
+    partial_list = loop_dir(partial_dir)
+    gt_list = loop_dir(gt_dir)
+
+    data_list = []
+    for partial, gt in zip(partial_list, gt_list):
+        data_list += [Data(pos=partial, y=gt)]
+    out = DataLoader(data_list, batch_size=32, shuffle=True)
+    return out
+
+
+def load_h5(filename):
     data = np.loadtxt(filename)
     cloud = o3d.io.read_point_cloud(filename)  # Read the point cloud
     o3d.visualization.draw_geometries([cloud])  # Visualize the point cloud
