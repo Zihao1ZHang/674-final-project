a1c537e352559904e7a2b2cb19118edc0b1db840
linlinlinL <50984992+linlinlinL@users.noreply.github.com>
2022-05-04 17:27:15 -0400

    test

--- model.py
+++ model.py
@@ -6,11 +6,9 @@ import numpy as np
 
 
 class Self_Attn(torch.nn.Module):
-    def __init__(self, in_dim, activation):
+    def __init__(self, in_dim):
         super(Self_Attn, self).__init__()
         self.chanel_in = in_dim
-        self.activation = activation
-
         self.query_conv = torch.nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
         self.key_conv = torch.nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
         self.value_conv = torch.nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
@@ -53,15 +51,14 @@ class PointNet(torch.nn.Module):
         super(PointNet, self).__init__()
 
 class FoldingBlock(torch.nn.Module):
-    def __init__(self, num_input_features, num_output_features):
+    def __init__(self, num_input_features):
         super(FoldingBlock, self).__init__()
         self.input = num_input_features
-        self.output = num_output_features
-        self.mlp1 = MLP([]) 000
-        self.mlp2 = MLP([]) 000
-        self.atten = Self_Attn() 000
+        self.mlp1 = MLP([2*num_input_features+2,2*num_input_features])
+        self.mlp2 = MLP([num_input_features,2*num_input_features])
+        self.atten = Self_Attn(2*num_input_features)
     def Up_module(self,points,grid):
-        ratio = self.output/self.input
+        ratio = 2
         dup = points.repeat(int(ratio),1).contiguous()
         point_1 = torch.cat((dup,grid),-1)
         point_2 = self.mlp1(point_1)
@@ -71,15 +68,16 @@ class FoldingBlock(torch.nn.Module):
         return point_4
 
     def Down_module(self, points,up_points):
-        point_1 = torch.reshape(up_points, (self.output, int(points.points.size(dim=1)*points.points.size(dim=0)/self.output))).contiguous()
+        x,y = up_points.shape
+        point_1 = torch.reshape(up_points, (int(x/2), y*2)).contiguous()
         point_2 = point_1-points
         point_3 = self.mlp2(point_2)
-        return  point_3
+        return point_3
 
     def forward(self, points, grid):
         up = self.Up_module(points,grid= grid)
         down = self.Down_module(points,up)
         up1 = self.Up_module(down,grid= grid)
-        print("1")
+        # print("1")
         return up1+up
 
