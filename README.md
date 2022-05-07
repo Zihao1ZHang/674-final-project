aec190de2dfd731fc9078a930268e9dad3503658
Gsh1707 <1405991853@qq.com>
2022-05-07 19:01:40 -0400

    Update

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# 674-final-project
-test
+SETUP
+
+Create a new python environment 3.6.13
+
+pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+
+cd .\kaolin\
+
+python .\setup.py develop
+
+cd ..
+
+cd .\emd\
+
+python .\setup.py install
+
+cd ..
+
+cd .\torch_packages\
+
+pip install .\torch_cluster-1.5.9-cp36-cp36m-win_amd64.whl
+
+pip install .\torch_scatter-2.0.9-cp36-cp36m-win_amd64.whl
+
+pip install .\torch_sparse-0.6.12-cp36-cp36m-win_amd64.whl
+
+pip install torch_geometric
+
+pip install h5py
+
+for visualization:
+
+pip install matplotlib
+
+conda install jupyter
+
+FINISH
+
+EMD: https://github.com/Colin97/MSN-Point-Cloud-Completion/tree/master/emd
+
+Kaolin: https://github.com/NVIDIAGameWorks/kaolin/
---
+++ train_AE.py
@@ -0,0 +1,46 @@
+import torch
+from torch_geometric.data import DataLoader
+from Compeletion3D import Completion3D
+from model import SA_net
+import kaolin
+from emd_func import *
+
+
+# from test import cal_chamfer_distance
+
+def train():
+    model.train()
+    total_loss = 0
+    step = 0
+    for data in train_loader:
+        if data.batch[-1] != 15:
+            continue
+        step += 1
+        data = data.to(device)
+        optimizer.zero_grad()
+        decoded = model(data, data.batch)
+        loss = torch.sum(emd(decoded.reshape(-1, 2048, 3), data.y.reshape(-1, 2048, 3), 0.002, 1000)[0]) + 10 * torch.sum(kaolin.metrics.pointcloud.chamfer_distance(decoded.reshape(-1, 2048, 3), data.y.reshape(-1, 2048, 3)))
+        loss.backward()
+        total_loss += loss.item() * data.num_graphs
+        optimizer.step()
+    return total_loss / len(dataset)
+
+
+if __name__ == '__main__':
+
+    dataset = Completion3D('../data/Completion3D', split='train', categories='Airplane')
+    print(dataset[0])
+    train_loader = DataLoader(
+        dataset, batch_size=16, shuffle=True)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = SA_net(1, 1).to(device)
+    # model.load_state_dict(torch.load('C:/Users/Gsh_1/OneDrive/Documents/GitHub/SA_Net/trained/SA_net_Ch_Car60.pt', map_location=device))
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    emd = emdModule()
+    print('Training started:')
+    for epoch in range(1, 121):
+        loss = train()
+        print('Epoch {:03d}, Loss: {:.4f}'.format(
+            epoch, loss))
+        if epoch % 20 == 0:
+            torch.save(model.state_dict(), './trained/SA_net_Ch_Airplane_674' + '{}'.format(epoch) + '.pt')
